891b1600cec80fde758894fccf49e96c4e986e34
袁仕崇 <xiaochong2154@163.com>
2015-04-23 22:42:35 +0800

    COMMINT

--- WSY_SWeather/WSY_SWeather/ViewController.swift
+++ WSY_SWeather/WSY_SWeather/ViewController.swift
@@ -74,6 +74,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 println("successRequest: \(url)")
                 var json = JSON(json!)
                 self.updateUISuccess(json)
+            } else {
+                self.loading.text = "Internet appears down! \n error: \(error?.description)"
+//                assert(error != nil, "request error \(error?.description)")
             }
         }
         
@@ -85,11 +88,97 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         self.loadingIndicator.stopAnimating()
         
         let service = WeatherService()
-        if let tempResult = json["city"]["country"].double {
-        
+        if let tempResult = json["list"][0]["main"]["temp"].double {
+            //get country
+            let country = json["city"]["country"].stringValue
+            //get and convert temperature
+            var temperature = service.convertTemperature(country, temperature: tempResult)
+            self.temperature.text = "\(temperature)"
+
+            //get city name
+            self.location.text = json["city"]["name"].stringValue
+            
+            //get and set icon
+            
+            let weather = json["list"][0]["weather"][0]
+            let condition = weather["id"].intValue
+            var icon = weather["icon"].stringValue
+            var nightTime = service.isNightTime(icon)
+            
+            service.updateWeatherIcon(condition, nightTime: nightTime, index: 0, callBack: self.updatePictures)
+            
+            //Get forecast
+            for index in 1...4 {
+                if let tempResult = json["list"][index]["main"]["temp"].double {
+                    //Get and convert temperature
+                    var temperature = service.convertTemperature(country, temperature: tempResult)
+                    if (index==1) {
+                        self.temp1.text = "\(temperature)°"
+                    }
+                    else if (index==2) {
+                        self.temp2.text = "\(temperature)°"
+                    }
+                    else if (index==3) {
+                        self.temp3.text = "\(temperature)°"
+                    }
+                    else if (index==4) {
+                        self.temp4.text = "\(temperature)°"
+                    }
+                    
+                    // Get forecast time
+                    var dateFormatter = NSDateFormatter()
+                    dateFormatter.dateFormat = "HH:mm"
+                    let rawDate = json["list"][index]["dt"].doubleValue
+                    let date = NSDate(timeIntervalSince1970: rawDate)
+                    let forecastTime = dateFormatter.stringFromDate(date)
+                    if (index==1) {
+                        self.time1.text = forecastTime
+                    }
+                    else if (index==2) {
+                        self.time2.text = forecastTime
+                    }
+                    else if (index==3) {
+                        self.time3.text = forecastTime
+                    }
+                    else if (index==4) {
+                        self.time4.text = forecastTime
+                    }
+                    
+                    // Get and set icon
+                    let weather = json["list"][index]["weather"][0]
+                    let condition = weather["id"].intValue
+                    var icon = weather["icon"].stringValue
+                    var nightTime = service.isNightTime(icon)
+                    service.updateWeatherIcon(condition, nightTime: nightTime, index: index, callBack: self.updatePictures)
+                    
+                } else {
+                    continue
+                }
+            }
+        } else {
+            self.loading.text = "Weather info is not available!"
         }
         
 //        let service = swiff
+    }
+    
+    func updatePictures(index: Int, name: String) {
+        if (index==0) {
+            self.icon.image = UIImage(named: name)
+        }
+        if (index==1) {
+            self.image1.image = UIImage(named: name)
+        }
+        if (index==2) {
+            self.image2.image = UIImage(named: name)
+        }
+        if (index==3) {
+            self.image3.image = UIImage(named: name)
+        }
+        if (index==4) {
+            self.image4.image = UIImage(named: name)
+        }
+
     }
     
     //MARK: - CLLocationManagerDelegate
--- WSY_SWeather/WSY_SWeather/WeatherService.swift
+++ WSY_SWeather/WSY_SWeather/WeatherService.swift
@@ -50,5 +50,92 @@ class WeatherService {
         }
     }
     
+    func convertTemperature(country: String, temperature: Double) -> Double {
+        if (country == "US") {
+            return round(((temperature - 273.15) * 1.8) + 32)
+        } else {
+            return round(temperature - 273.15)
+        }
+    }
+    
+    func isNightTime(icon: String) -> Bool {
+        return icon.rangeOfString("n") != nil
+    }
+    
+    func updateWeatherIcon(condition: Int, nightTime: Bool, index: Int, callBack: (index: Int, name: String) -> ()) {
+    
+        //ThunderStorm
+        if condition < 300 {
+            if nightTime {
+                callBack(index: index, name: "tstorm1_night")
+            } else {
+                callBack(index: index, name: "tstorm1")
+            }
+        }
+        
+        //Drizzle
+        else if condition < 500 {
+            callBack(index: index, name: "light_rain")
+        }
+        //Rain / Freezing rain /Shower rain
+        else if condition < 600 {
+            callBack(index: index, name: "shower3")
+        }
+        //snow
+        else if condition < 700 {
+            callBack(index: index, name: "snow4")
+        }
+        // Fog / Mist / Haze / etc.
+        else if (condition < 771) {
+            if nightTime {
+                callBack(index: index, name: "fog_night")
+            } else {
+                callBack(index: index, name: "fog")
+            }
+        }
+            // Tornado / Squalls
+        else if (condition < 800) {
+            callBack(index: index, name: "tstorm3")
+        }
+            // Sky is clear
+        else if (condition == 800) {
+            if (nightTime){
+                callBack(index: index, name: "sunny_night")
+            }
+            else {
+                callBack(index: index, name: "sunny")
+            }
+        }
+            // few / scattered / broken clouds
+        else if (condition < 804) {
+            if (nightTime){
+                callBack(index: index, name: "cloudy2_night")
+            }
+            else{
+                callBack(index: index, name: "cloudy2")
+            }
+        }
+            // overcast clouds
+        else if (condition == 804) {
+            callBack(index: index, name: "overcast")
+        }
+            // Extreme
+        else if ((condition >= 900 && condition < 903) || (condition > 904 && condition < 1000)) {
+            callBack(index: index, name: "tstorm3")
+        }
+            // Cold
+        else if (condition == 903) {
+            callBack(index: index, name: "snow5")
+        }
+            // Hot
+        else if (condition == 904) {
+            callBack(index: index, name: "sunny")
+        }
+            // Weather condition is not available
+        else {
+            callBack(index: index, name: "dunno")
+        }
+    }
+    
     
 }
